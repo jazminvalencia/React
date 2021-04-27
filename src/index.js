7a33aaf1ab82e7728cf5b4517f3e02fdd2ad8771
jaz valencia <angelvalencia9811@gmail.com>
2021-04-27 12:25:44 -0600

    primer componente y aplicacion de estilos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/ const element = document.createElement('h1');
+// const element = document.createElement('h1');
 // element.innerText = 'Hello, Platzi Badges!';
 
 // const container = document.getElementById('app');
@@ -7,20 +7,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Badge from './components/Badge';
 
-var name = "richart"
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 
-//const xjs = <h1>hola, Platzi blogers</h1>
-//const element = React.createElement('a',{ href: 'https://www.platzi.com' },'link a platzi');
-
-//const xjs = <h1>hola, soy {name}</h1>
-//const element = React.createElement('h1',{}, `hola soy ${name}`)
-
-const jsx = (<div>
-    <h1>Soy Jazmin</h1>
-    <p>soy ingeniero frontend</p>
-</div>)
 const container = document.getElementById('app');
 
-// ReactDOM.render(_qué, __dónde_);
-ReactDOM.render(jsx, container);
+ReactDOM.render(<Badge />, container);
