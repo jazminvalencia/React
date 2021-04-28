b0482a69a46a0dd33d666717d43cb235a58395e1
Angel Jazmin <ajvalenciab@concredito.com.mx>
2021-04-28 12:18:43 -0600

    practica de cap17, teoria y mas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BadgeNew from './pages/BadgesNew';
 import Badges from './pages/Badges';
+import App from './components/App'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badges/>, container);
+ReactDOM.render(<App/>, container);
