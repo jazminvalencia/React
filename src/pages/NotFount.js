be63050c8207b66ab3af9d599921b11ea71969b2
Angel Jazmin <ajvalenciab@concredito.com.mx>
2021-04-29 08:46:06 -0600

    Reto cap 20 terminado

--- src/pages/NotFount.js
+++ src/pages/NotFount.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Link} from 'react-router-dom';
+import error from '../images/astronauta.jpg'
+import '../components/style/NotFount.css'
 
 function NotFount(props) {
-    return <h1>404: Not Found </h1>
+    return (
+        <React.Fragment>
+            <div className="Nofound-back">
+                <img className="img-fluid" src={error} alt="Logo"></img>
+                <h1 className="title-noFount">404: Pagina no encontrada</h1>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default NotFount;
